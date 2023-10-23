88b31549b94d93b97f3427e655ee990a53abac08
nsurve@clemson.edu <namratasurve18@gmail.com>
2023-10-23 12:49:51 -0400

    first commit

---
+++ pumpkin puzzler/assets/js/form.js
@@ -0,0 +1,25 @@
+/* this file is used by contact.html to store firstname submitted in form into sessionStorage, 
+so form-success.html can get it from sessionStorage for form success message */
+
+/**
+ * when submit event happens, run handleSubmit function
+ */
+function initialiseForm() {
+    document.getElementById("contact-form").addEventListener("submit", handleSubmit);
+}
+
+/**
+ * get the firstname from form, set it as sessionStorage item to be used by form success page, then submit form
+ * @param {event} event submit
+ */
+function handleSubmit(event) {
+    event.preventDefault();
+    let name = document.getElementById("first-name");
+    window.sessionStorage.setItem("name", name.value);
+    document.getElementById("contact-form").submit();
+}
+
+//when DOM has loaded, listen for submit on the form
+document.addEventListener("DOMContentLoaded", function () {
+    initialiseForm();
+});
