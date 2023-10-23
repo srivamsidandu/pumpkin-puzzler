aba3791a3b78df097f7950aad1f0a9c04707b687
nsurve180 <142470513+nsurve180@users.noreply.github.com>
2023-10-23 18:16:41 -0400

    Delete word-monster-master directory

--- word-monster-master/assets/js/utils.js
+++
@@ -1,8 +0,0 @@
-// utils.js
-
-import { words } from './words.js';
-
-export function getRandomWord() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  return words[randomIndex];
-}
